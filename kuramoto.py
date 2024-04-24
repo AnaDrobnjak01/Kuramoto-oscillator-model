62195fea5300ac8a364f2cb31765250a61c3bdd4
Ana <mihajloviccana37@gmail.com>
2024-04-24 23:26:27 +0200

    added slider and button

--- kuramoto.py
+++ kuramoto.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.widgets as widgets
 
 #ovo racuna vektor izvoda (tetaprim_1, ..., tetaprim_n)
 def kuramoto(theta, K, omega):
@@ -13,16 +14,15 @@ def kuramoto(theta, K, omega):
     dtheta_dt[i] = omega[i] + K/N * sum
   return dtheta_dt
 
-print("Input parameters for differential equation : ")
-N = int(input("number of oscillators : "))
-timesteps = int(input("simulation time : ")) # vreme simulacije
-h = float(input("timestep : "))
-K = float(input("strenght of coupling : ")) # jacina uticaja medju oscilatorima
+N = 10 # broj oscilatora
+timesteps = 1000 # vreme simulacije
+h = 0.001 # korak
+K = 100 # jacina uticaja medju oscilatorima
 
 theta = np.random.uniform(0, 2 * np.pi, N)
 # omega = np.random.uniform(0.5, 2, N)
-omega_value = float(input("angle speed : "))
-omega = np.repeat(omega_value, N)
+# omega_value = float(input("angle speed : "))
+omega = np.repeat(5, N)
 
 fig, ax = plt.subplots()
 ax.set_xlim(-1.2, 1.2)
@@ -50,8 +50,62 @@ def update(frame):
     oscillators.set_data(x, y)
     return oscillators,
 
+def resetSimulation(event):
+    global theta
+    theta = theta = np.random.uniform(0, 2 * np.pi, N)
+
+def updateK(val):
+  global K 
+  K = sliderK.val 
+
+def updateOmega(val):
+  global omega, N
+  omega = np.fill(sliderOmage.val, N)
+
+def updateN(val):
+  global N 
+  N = sliderN.val
+
+
 oscillators, = ax.plot([], [], 'bo')
 
 ani = animation.FuncAnimation(fig, update, frames=int(timesteps/h), init_func=init, blit=True, interval=50)
+
+#treba podesiti ovo da vizuelno izgleda pristojno sad samo dodajem da radi 
+axes = plt.axes([0.81, 0.000001, 0.1, 0.075])
+btResetSim = widgets.Button(axes, 'Reset simulation',color="yellow")
+btResetSim.on_clicked(resetSimulation)
+
+axesK = plt.axes([0.8, 0.8, 0.2, 0.2])
+sliderK = widgets.Slider(axesK, 'strenght of coupling', 0.1, 10)
+sliderK.on_changed(updateK)
+
+
+
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
